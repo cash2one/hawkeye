f0a20e01a5f0373c84591138cc7cc9436790e054
feix.chow <feix@feixMacBook.local>
2016-05-27 23:02:31 +0800

    subdomain parser

---
+++ subdomain_parser.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+""" subdomain """
+
+__author__ = 'feix.chow'
+
+import re
+import BeautifulSoup
+
+from parser import Parser
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+
+def google_search_handler(text):
+    soup = BeautifulSoup.BeautifulSoup(text)
+    contents = soup.findAll('cite', attrs={'class': '_Rm'})
+    results = []
+    for content in contents:
+        domain = content.contents[0].replace('/', '')
+        results.append({'domain': domain})
+    return results
+
+
+def baidu_search_handler(text):
+    soup = BeautifulSoup.BeautifulSoup(text)
+    results = []
+    contents = soup.findAll('div', attrs={'class': 'result c-container '})
+    for content in contents:
+        content = content.find('a', attrs={'class': 'c-showurl'})
+        domain = content.contents[0].split('/')[0]
+        domain = domain.replace('<em>', '').replace('</em>', '')
+        results.append({'domain': domain})
+    return results
+
+
+def sogou_search_handler(text):
+    soup = BeautifulSoup.BeautifulSoup(text)
+    contents = soup.findAll('div', attrs={'class': 'r-sech site_query'})
+    results = []
+    for content in contents:
+        domain = content.find('a').get('href').split('/')[2]
+        results.append({'domain': domain})
+    return results
+
+
+def result_handler(keyword, result):
+    return result['domain']
+
+
+def main():
+    keywords = ['wooyun.org']
+    keywords = ('site:' + keyword for keyword in keywords)
+    sogou_parser = Parser(protocol='https', referer='https://www.sogou.com/')
+    search_handler = sogou_search_handler
+    from url_constructor import sogou_url_constructor as url_constructor
+    results = sogou_parser.start(keywords=keywords, url_constructor=url_constructor, search_handler=search_handler, result_handler=result_handler)
+    print set(results)
+
+
+if __name__ == '__main__':
+    main()
--- url_constructor.py
+++ url_constructor.py
@@ -11,5 +11,17 @@ def github_url_constructor(keyword, indexs=101):
 
 def baidu_url_constructor(keyword, indexs=50):
     url_base = 'http://www.baidu.com/s?pn={index}&wd={keyword}'
-    for index in range(0, index):
+    for index in range(0, indexs):
         yield (keyword, url_base.format(index=index * 10, keyword=keyword))
+
+
+def google_url_constructor(keyword, indexs=50):
+    url_base = 'https://www.google.com/search?num=50&start={index}&q={keyword}'
+    for index in range(0, indexs):
+        yield (keyword, url_base.format(index=index, keyword=keyword))
+
+
+def sogou_url_constructor(keyword, indexs=50):
+    url_base = 'https://www.sogou.com/web?page={index}&query={keyword}'
+    for index in range(0, indexs):
+        yield (keyword, url_base.format(index=index, keyword=keyword))
